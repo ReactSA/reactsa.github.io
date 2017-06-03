5dccdd299b7aeeca8194131f82b1fd886dc53cc8
melodytempleton <mtempletoncantu@gmail.com>
2017-06-03 13:07:05 -0500

    Add initial React development environment and structure Refactor top banner into a component Add React logo to TopBanner Change styling for H1 and p in TopBanner

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+    entry: './src/index.js', // entry point
+    output: {// place where bundled app will be served
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true, // autorefresh
+        port: 8080 // development port server
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/, // search for js files
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react'] // use es2015 and react
+                }
+            }
+        ]
+    }
+}
+module.exports = config;
