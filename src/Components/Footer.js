24256cf44fac08ae2e843eb3750e3e5768630631
melodytempleton <mtempletoncantu@gmail.com>
2017-08-02 21:42:47 -0500

    Change webpage to React set-up Add logo to top banner Make a few other slight formatting changes

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+/**
+ * Created by melodytempleton on 8/2/17.
+ */
+import React from 'react';
+
+
+const Footer = () =>{
+
+    return(
+
+        <footer className="footer">
+            <div className="container">
+                <p className="footer-text pull-right">ReactSA &copy; 2017 <a href="https://www.grok-interactive.com">Grok Interactive</a></p>
+            </div>
+        </footer>
+    );
+
+};
+
+export default Footer;
---
+++ src/Components/MarketingRowRight.js
@@ -0,0 +1,33 @@
+/**
+ * Created by melodytempleton on 8/2/17.
+ */
+import React from 'react';
+
+const MarketingRowRight = () => {
+
+    return (
+        <div className="col-lg-6">
+            <h2>Contribute to the Code / Discussion</h2>
+            <p>Help the community by contributing to all things React in San Antonio, <em>including this page</em>.
+
+                Join <a href="https://github.com/ReactSA">ReactSA on GitHub</a></p>
+
+            <h3>Join the Slack Channel</h3>
+            <p>The discussion never ends in the <a href="http://slack.reactsa.com/">ReactSA Slack Channel</a>. If you have a project you'd like to have added to our Github, please post it here.  Join us today!</p>
+
+            <h3>Follow our Twitter</h3>
+            <p>To keep up with local news and event information on React in San Antonio, please follow us on Twitter at <a href="https://twitter.com/React_SA">@React_SA</a></p>
+
+            <h3>tl;dr</h3>
+            <p>
+                <strong>GitHub</strong> &mdash; <a href="https://github.com/ReactSA">https://github.com/ReactSA</a><br/>
+                <strong>Twitter</strong> &mdash; <a href="https://twitter.com/React_SA">https://twitter.com/React_SA</a><br/>
+                <strong>Meetup</strong> &mdash; <a href="https://www.meetup.com/ReactSA/">https://www.meetup.com/ReactSA</a><br/>
+                <strong>Slack</strong> &mdash; <a href="http://slack.reactsa.com/">Join via Invite Link</a>
+            </p>
+        </div>
+
+    );
+};
+
+export default MarketingRowRight;
